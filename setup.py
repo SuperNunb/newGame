ce52ee9383a7ae840afab8fa5b784cf6453bdb33
SuperNunb <33640719+SuperNunb@users.noreply.github.com>
2020-04-03 17:32:36 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from cx_Freeze import setup, Executable 
+from cx_Freeze import setup, Executable
+import sys
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 setup(name = "NewGame" , 
     version = "1.0" ,
     options={"build_exe": {"packages":["pygame", "time", "random", "os"],
         "include_files":[
             "img/avatars.png", "img/backgroundSlice.png", "img/baddies.png", 
-            "img/other.png", "img/statics.png", "audio/levels.wav", "audio/menu.wav", "audio/boss.wav"
+            "img/other.png", "img/statics.png", "audio/grapple.wav", "audio/jump.wav", "audio/powerup.wav", "audio/gun.wav", "audio/damage.wav", "audio/parachute.wav", "audio/glass.wav", "audio/levels.wav", "audio/menu.wav", "audio/boss.wav", "audio/wind.wav", "vt323.ttf", "userPrefs"
         ]}},
     executables = [Executable("main.py")])
